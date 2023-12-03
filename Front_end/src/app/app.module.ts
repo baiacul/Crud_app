7f061f294c123b38e8f04f100d580a422296d396
baiacul <kaique.kpfp@gmail.com>
2023-12-03 01:52:54 -0300

    configurando o bootstrap e comecando o design do web

--- Front_end/src/app/app.module.ts
+++ Front_end/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraPesquisaComponentComponent } from './components/barra-pesquisa-component/barra-pesquisa-component.component';
+import { InformacoesComponentComponent } from './components/informacoes-component/informacoes-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraPesquisaComponentComponent,
+    InformacoesComponentComponent
   ],
   imports: [
     BrowserModule,
