181a2a4d0cd5de40c30d01f3cfa86c95008f4e7c
baiacul <kaique.kpfp@gmail.com>
2023-12-03 22:33:37 -0300

    criação da parte de cirar produto

--- Front_end/src/app/app.module.ts
+++ Front_end/src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BarraPesquisaComponentComponent } from './components/barra-pesquisa-component/barra-pesquisa-component.component';
 import { InformacoesComponentComponent } from './components/informacoes-component/informacoes-component.component';
 import { AddComponentComponent } from './components/add-component/add-component.component';
 import { DetailsComponent } from './components/details/details.component';
+import { CurrencyMaskConfig, CurrencyMaskModule, CURRENCY_MASK_CONFIG } from 'ng2-currency-mask';
+
 
 @NgModule({
   declarations: [
@@ -24,10 +24,21 @@ import { DetailsComponent } from './components/details/details.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    CurrencyMaskModule
     
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export const CustomCurrencyMaskConfig: CurrencyMaskConfig = {
+  align: "right",
+  allowNegative: true,
+  decimal: ",",
+  precision: 2,
+  prefix: "R$ ",
+  suffix: "",
+  thousands: "."
+};
--- Front_end/src/app/components/add-component/add-component.component.ts
+++ Front_end/src/app/components/add-component/add-component.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Produtos } from '../../models/produtos.model';
+import { ProdutosService } from '../../services/produtos.service';
 
 @Component({
   selector: 'app-add-component',
   templateUrl: './add-component.component.html',
   styleUrl: './add-component.component.css'
 })
-export class AddComponentComponent {
+export class AddComponentComponent implements OnInit {
 
+  ngOnInit(): void { }
+
+  produto: Produtos ={
+    nome_produto: '',
+    categoria_id: 0,
+    valor_produto: 0,
+    data_vencimento: '',
+    quantidade_estoque: 0,
+    produto_perecivel: false,
+
+  }
+  submitted = false;
+  constructor(private produtoService: ProdutosService){}
+  saveproduto(){
+    const data = {
+      nome_produto: this.produto.nome_produto,
+      categoria_id: this.produto.categoria_id,
+      valor_produto: this.produto.valor_produto,
+      data_vencimento: this.produto.data_vencimento,
+      quantidade_estoque: this.produto.quantidade_estoque,
+      produto_perecivel: this.produto.produto_perecivel,
+    };
+
+  
+    this.produtoService.create(data)
+    .subscribe({
+    next: (res) => {
+      console.log(res);
+      this.submitted = true;
+    },
+    error: (e) => console.log(e)
+    });
+
+  }
+
+  novoProduto(): void {
+    this.submitted = false;
+    this.produto = {
+      nome_produto: '',
+      categoria_id: 0,
+      valor_produto: 0,
+      data_vencimento: '',
+      quantidade_estoque: 0,
+      produto_perecivel: false,
+    };
+  }
+
+  
 }
+
