55678bf032f47237aaa8ca0f6bd2c3b4ab6aaedd
baiacul <kaique.kpfp@gmail.com>
2023-12-04 04:09:06 -0300

    configuração da lista

--- Front_end/src/app/components/details/details.component.ts
+++ Front_end/src/app/components/details/details.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+// No seu componente TypeScript
+import { Component, OnInit } from '@angular/core';
+import { Produtos } from '../../models/produtos.model';
+import { ProdutosService } from '../../services/produtos.service';
+import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrl: './details.component.css'
+  styleUrls: ['./details.component.css'] // Correção aqui
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
 
+  produtos: Produtos[] = [];
+
+  aberto = false;
+  produto_selecionado: any = null;
+
+  constructor(private produtoService: ProdutosService) { }
+
+  ngOnInit(): void {
+    this.AtualizarProdutos();
+  }
+
+  AtualizarProdutos(): void {
+    this.produtoService.getAll()
+      .subscribe({
+        next: (data) => {
+          this.produtos = data;
+          console.log(data);
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  extender(produto: Produtos): void {
+    this.produto_selecionado = produto.nome_produto;
+    this.aberto = !this.aberto;
+    console.log(this.produto_selecionado);
+
+  }
+  delete(produto: Produtos): void {
+    this.produto_selecionado = produto.nome_produto
+    this.produtoService.delete(this.produto_selecionado)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.AtualizarProdutos();
+        },
+        error: (e) => console.log(e)
+      })
+  }
+  edit(produto:Produtos):void{}
 }
